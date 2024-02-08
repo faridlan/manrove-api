b175dc56388cca7671f9f8fa09f2186eb92217d6
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-02-08 23:17:29 +0700

    [ADD] migrations for legal_category

---
+++ config/db/migrations/20240208155203_create_table_legal_category.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE
+    legal_category (
+        id VARCHAR DEFAULT
+        REPLACE (
+                uuid_generate_v4():: text,
+                '-',
+                ''
+            ) NOT NULL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            created_at BIGINT NOT NULL,
+            updated_at BIGINT NOT NULL,
+            deleted_at TIMESTAMP NULL
+    );
