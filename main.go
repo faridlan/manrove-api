f8c438e50be24be023775e8b6751e0590fc70257
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-02-08 23:22:00 +0700

    [ADD] route / endpoint for legal_category feature

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/nostracode/mangrove-api/config/db/conn"
 	financecontroller "github.com/nostracode/mangrove-api/controller/finance_controller"
+	legalcategorycontroller "github.com/nostracode/mangrove-api/controller/legal_category_controller"
 	rolecontroller "github.com/nostracode/mangrove-api/controller/role_controller"
 	usercontroller "github.com/nostracode/mangrove-api/controller/user_controller"
 	"github.com/nostracode/mangrove-api/exception"
 	"github.com/nostracode/mangrove-api/helper"
 	financerepo "github.com/nostracode/mangrove-api/repository/finance_repo"
+	legalcategoryrepo "github.com/nostracode/mangrove-api/repository/legal_category_repo"
 	rolerepo "github.com/nostracode/mangrove-api/repository/role_repo"
 	userrepo "github.com/nostracode/mangrove-api/repository/user_repo"
 	financeservice "github.com/nostracode/mangrove-api/service/finance_service"
+	legalcategoryservice "github.com/nostracode/mangrove-api/service/legal_category_service"
 	roleservice "github.com/nostracode/mangrove-api/service/role_service"
 	userservice "github.com/nostracode/mangrove-api/service/user_service"
 )
@@ -37,6 +40,11 @@ func main() {
 	financeRepo := financerepo.NewFinanceRepository()
 	financeService := financeservice.NewFinanceRepository(financeRepo, db, validator)
 	financeController := financecontroller.NewFinanceController(financeService)
+
+	//Legal Category
+	legalCategoryRepo := legalcategoryrepo.NewLegalCategoryRepository()
+	legalCategoryService := legalcategoryservice.NewLegalCategoryService(legalCategoryRepo, db, validator)
+	legalCategoryController := legalcategorycontroller.NewLegalCategoryController(legalCategoryService)
 
 	// Create a new Fiber instance
 	app := fiber.New(
@@ -65,6 +73,13 @@ func main() {
 	app.Get("/api/finances/:financeId", financeController.FindById)
 	app.Put("/api/finances/:financeId", financeController.Update)
 	app.Delete("/api/finances/:financeId", financeController.Delete)
+
+	//Endpoint Of Legal Category
+	app.Post("/api/legal/categories", legalCategoryController.Create)
+	app.Get("/api/legal/categories", legalCategoryController.FindAll)
+	app.Get("/api/legal/categories/:legalCategoryId", legalCategoryController.FindById)
+	app.Put("/api/legal/categories/:legalCategoryId", legalCategoryController.Update)
+	app.Delete("/api/legal/categories/:legalCategoryId", legalCategoryController.Delete)
 
 	// Start the Fiber app on port 3030
 	err := app.Listen(":3030")
